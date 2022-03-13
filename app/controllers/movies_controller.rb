16fd852fe26c620d0ebdb12bbb4145fb6e096bfd
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-12 23:22:03 -0800

    adds create, update, destroy routes and methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,29 @@ class MoviesController < ApplicationController
     render json: movie.as_json
   end
 
+  def create
+    movie = Movie.new(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot]
+    )
+    movie.save
+    render json: movie.as_json
+  end
+
+  def update
+    movie = Movie.find_by(id: params[:id])
+    movie.title = params[:title] || movie.title
+    movie.year = params[:year] || movie.year
+    movie.plot = params[:plot] || movie.plot
+    movie.save
+    render json: movie.as_json
+  end
+
+  def destroy
+    movie = Movie.find_by(id: params[:id])
+    movie.destroy
+    render json: "Your movie was deleted!".as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get "/all-actors" => "actors#list_all"
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
+  post "/movies" => "movies#create"
+  patch "/movies/:id" => "movies#update"
+  delete "/movies/:id" => "movies#destroy"
 end
