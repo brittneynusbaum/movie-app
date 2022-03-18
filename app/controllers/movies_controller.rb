6bed0988816e84bc0a0d9ac69cd352a177e028e5
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 20:31:09 -0700

    adds happy/sad paths to create and update methods in movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,8 +18,11 @@ class MoviesController < ApplicationController
       director: params[:director],
       english: params[:english]
     )
-    movie.save
-    render json: movie.as_json
+    if movie.save
+      render json: movie.as_json
+    else
+      render json: {message: movie.errors.full_messages}
+    end
   end
 
   def update
@@ -29,8 +32,12 @@ class MoviesController < ApplicationController
     movie.plot = params[:plot] || movie.plot
     movie.director = params[:director] || movie.director
     movie.english = params[:english] || movie.english
-    movie.save
-    render json: movie.as_json
+    
+    if movie.save
+      render json: movie.as_json
+    else
+      render json: {message: movie.errors.full_messages}
+    end
   end
 
   def destroy
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
+  validates :title, length: { minimum: 2 }
+  validates :year, length: { in: 4.. 4 }
   validates :director, presence: true
   validates :plot, length: { in: 10.. 500 }
 end
